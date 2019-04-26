8aca345182ee168f75355777bee658bd72347604
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 01:03:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   output = ""
-  if array.count = 1
+  if array.count == 1
     output = array[0]
-  elsif array.count = 2
+  elsif array.count == 2
     output = array.join( " and " )
   else
     output << "#{array[0]}, "
