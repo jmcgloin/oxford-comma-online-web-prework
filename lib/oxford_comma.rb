2761e765722f3f894869eb03e870a4a5ab88c2ce
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 00:52:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
 def oxford_comma(array)
   output = ""
-  array.each { |item| 
+  array.each_index { |index| if index == 0  then output << array[index] elsif index < array.count -1 then output << ", #{array[index]}" else output << ", and #{array[index]}" }
 end
